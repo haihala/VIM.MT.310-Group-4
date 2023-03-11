1049afd49adbbf24397d9a7e5240679916389e73
Eero Häihälä <eero.haihala@gmail.com>
2023-03-11 09:22:09 +0200

    Make camera rotate up and down

--- Unity project/Assets/Scripts/Character.cs
+++ Unity project/Assets/Scripts/Character.cs
@@ -6,20 +6,32 @@ using UnityEngine.InputSystem;
 public class Character : MonoBehaviour
 {
     public CharacterController characterController;
-    public PlayerInput playerInput;
+    public Transform cameraPivot;
 
     public float movementSpeed;
     public float cameraSpeed;
     public float jumpImpulse;
     public float gravity;
+    public float cameraClamp;
 
     Vector2 inputMovement;
     Vector2 cameraRotation;
     Vector3 velocity;
 
+    float rotX;
+    float rotY;
+
+    void Start()
+    {
+        rotX = cameraPivot.localRotation.eulerAngles.x;
+    }
+
     void Update()
     {
+        // Movement
         velocity = new Vector3(0, velocity.y, 0);
+        // This does rely on the facing of the player object, which is rotated accordingly,
+        // but once a model has been added, it will look weird as the model will rotate in place when you turn the camera.
         velocity += transform.right * movementSpeed * inputMovement.x;
         velocity += transform.forward * movementSpeed * inputMovement.y;
         if (!characterController.isGrounded)
@@ -28,7 +40,13 @@ public class Character : MonoBehaviour
         }
 
         characterController.Move(velocity * Time.deltaTime);
-        transform.Rotate(0, cameraSpeed * cameraRotation.x, 0);
+
+        // Rotation
+        transform.Rotate(0, cameraRotation.x * cameraSpeed * Time.deltaTime, 0);
+        rotX -= cameraRotation.y * cameraSpeed * Time.deltaTime;
+        rotX = Mathf.Clamp(rotX, -cameraClamp, cameraClamp);
+
+        cameraPivot.localRotation = Quaternion.Euler(rotX, rotY, 0.0f);
     }
 
     public void OnMovement(InputAction.CallbackContext value)
