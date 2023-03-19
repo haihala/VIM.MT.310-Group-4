745fd02f0d1377e029d6152b8804aea301de927e
Eero Häihälä <eero.haihala@gmail.com>
2023-03-19 11:18:52 +0200

    Add a guard that patrols around obstacles

---
+++ Unity project/Assets/NPCs/Guard.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Guard : MonoBehaviour
+{
+    public List<Transform> targets;
+    int targetIndex = -1;
+
+    NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void GotoNextPoint()
+    {
+        targetIndex = (targetIndex + 1) % targets.Count;
+        agent.destination = targets[targetIndex].position;
+    }
+
+
+    void Update()
+    {
+        // Choose the next destination point when the agent gets
+        // close to the current one.
+        if (!agent.pathPending && agent.remainingDistance < 0.5f)
+        {
+            GotoNextPoint();
+        }
+    }
+}
