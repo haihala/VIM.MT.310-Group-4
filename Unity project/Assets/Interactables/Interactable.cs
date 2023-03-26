11f4b0da9459430d35ee23ba195e2794ad5c723f
Eero Häihälä <eero.haihala@gmail.com>
2023-03-26 09:35:07 +0300

    Improve outlines on interactables

--- Unity project/Assets/Interactables/Interactable.cs
+++ Unity project/Assets/Interactables/Interactable.cs
@@ -5,21 +5,35 @@ using UnityEngine;
 [RequireComponent(typeof(Outline))]
 public abstract class Interactable : MonoBehaviour
 {
+    [SerializeField]
+    float activeoutlineThickness = 6;
+    [SerializeField]
+    Color activeoutlineColor = Color.yellow;
+
+    [SerializeField]
+    float inActiveoutlineThickness = 4;
+    [SerializeField]
+    Color inActiveoutlineColor = Color.white;
     Outline outline;
 
     protected virtual void Start()
     {
         outline = GetComponent<Outline>();
+        outline.OutlineMode = Outline.Mode.OutlineVisible;
+        outline.enabled = true;
+        UnHighlight();
     }
 
     public virtual void Highlight()
     {
-        outline.enabled = true;
+        outline.OutlineColor = activeoutlineColor;
+        outline.OutlineWidth = activeoutlineThickness;
     }
 
     public virtual void UnHighlight()
     {
-        outline.enabled = false;
+        outline.OutlineColor = inActiveoutlineColor;
+        outline.OutlineWidth = inActiveoutlineThickness;
     }
 
     public abstract void OnInteract(GameObject player);
