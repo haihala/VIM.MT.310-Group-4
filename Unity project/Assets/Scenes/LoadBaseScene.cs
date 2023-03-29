381acb1a4c3295e7eb968b5de6609cf9b917a9fa
Eero Häihälä <eero.haihala@gmail.com>
2023-03-29 20:38:40 +0300

    Refine multi-scene setup

---
+++ Unity project/Assets/Scenes/LoadBaseScene.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadBaseScene : MonoBehaviour
+{
+    void Awake()
+    {
+        SceneManager.LoadScene("Base", LoadSceneMode.Additive);
+    }
+}
