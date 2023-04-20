741dac3a85992f883cf3c44834675df74548a6c9
Eero Häihälä <eero.haihala@gmail.com>
2023-04-20 20:29:19 +0300

    Fix timer

--- Unity project/Assets/Scenes/Timer.cs
+++ Unity project/Assets/Scenes/Timer.cs
@@ -9,16 +9,18 @@ public class Timer : MonoBehaviour
     int seconds = 300;
     TextMeshProUGUI text;
     SceneChanger sceneChanger;
+    int target;
 
     void Start()
     {
+        target = (int)Time.time + seconds;
         text = GetComponent<TextMeshProUGUI>();
         sceneChanger = GetComponent<SceneChanger>();
     }
 
     void Update()
     {
-        int value = seconds - (int)Time.time;
+        int value = target - (int)Time.time;
         text.text = value.ToString();
         if (value <= 0)
         {
